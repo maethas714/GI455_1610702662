9c92841c8085f1c029fb41d3c545c31e7d994c15
DESKTOP-101TCDR\maeth <maethas714@gmail.com>
2021-01-28 03:07:17 +0700

    SendAssignmentWeek1

--- Assigment Week1/Assets/Code1.cs
+++ Assigment Week1/Assets/Code1.cs
@@ -9,36 +9,68 @@ public class Code1 : MonoBehaviour
     public GameObject inputField;
     public GameObject textDisplay;
     
-    public void storeName()
+
+    //public void storeName()
+    //{
+
+    //    name = inputField.GetComponent<Text>().text;
+    //    if (name == "Mango")
+    //    {
+    //        textDisplay.GetComponent<Text>().text = "[  " + name + " ]is Found";
+
+    //    }
+    //    else if (name == "Computer")
+    //    {
+    //        textDisplay.GetComponent<Text>().text = "[" + name + "]is Found";
+    //    }
+    //    else if (name == "Google")
+    //    {
+    //        textDisplay.GetComponent<Text>().text = "[" + name + "]is Found";
+    //    }
+    //    else if (name == "Uncharted4")
+    //    {
+    //        textDisplay.GetComponent<Text>().text = "[" + name + "]is Found";
+    //    }
+    //    else if (name == "Unreal")
+    //    {
+    //        textDisplay.GetComponent<Text>().text = "[" + name + "]is Found";
+    //    }
+    //    else if (name != "Mango" + "Computer" + "Google" + "Uncharted4" + "Unreal")
+    //    {
+    //        textDisplay.GetComponent<Text>().text = "[" + name + "]is not Found";
+    //    }
+
+    //}
+    public void buyName()
     {
-
         name = inputField.GetComponent<Text>().text;
-        if(name == "Mango")
+        
+        switch (name)
         {
-            textDisplay.GetComponent<Text>().text = "[" + name + "]is Found";
-            
+            case "Mango":
+                if (name == "Mango")
+                textDisplay.GetComponent<Text>().text = "[ <color=green>" + name + "</color> ] is found";
+                break;
+            case "Computer":
+                if (name == "Computer")
+                    textDisplay.GetComponent<Text>().text = "[ <color=green>" + name +"</color>] is Found";
+                break;
+            case "Google":
+                if (name == "Google")
+                textDisplay.GetComponent<Text>().text = "[ <color=yellow>" + name + "</color>] is Found";
+                break;
+            case "Uncharted4":
+                if (name == "Uncharted4")
+                textDisplay.GetComponent<Text>().text = "[ <color=red>" + name + "</color>] is Found";
+                break;
+            case "Unreal":
+                if (name == "Unreal") 
+                textDisplay.GetComponent<Text>().text = "[ <color=green>" + name + "</color>] is Found";
+                break;
+            default:
+                textDisplay.GetComponent<Text>().text = "[ <color=orange>" + name + "</color>] is not Found";
+                break;
         }
-        else if (name == "Computer")
-        {
-            textDisplay.GetComponent<Text>().text = "[" + name + "]is Found";
-        }
-        else if (name == "Google")
-        {
-            textDisplay.GetComponent<Text>().text = "[" + name + "]is Found";
-        }
-        else if (name == "Uncharted4")
-        {
-            textDisplay.GetComponent<Text>().text = "[" + name + "]is Found";
-        }
-        else if (name == "Unreal")
-        {
-            textDisplay.GetComponent<Text>().text = "[" + name + "]is Found";
-        }
-        else if (name != "Mango"+ "Computer"+"Google"+"Uncharted4"+"Unreal")
-        {
-            textDisplay.GetComponent<Text>().text = "[" + name + "]is not Found";
-        }
-
     }
-    
+
 }
